93eeadf2c5e6538793585579eeb2511b125275c3
futurelivealways <futurelivealways@gmail.com>
2026-07-03 15:34:15 +0530

    Add SEO - sitemap, meta tags, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
-import vercel from '@astrojs/vercel';
-
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://theclosereport.com',
   output: 'server',
-  integrations: [react()],
-
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  adapter: vercel()
+  }
 });
